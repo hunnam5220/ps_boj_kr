e74b193b2b899fe2ad713b21391f8e3e9c09d1a9
juuuun_b <31687036+hunnam5220@users.noreply.github.com>
2023-04-07 17:51:28 +0900

    [Silver III] Title: 게으른 백곰, Time: 564 ms, Memory: 50392 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
@@ -2,19 +2,18 @@ from sys import stdin
 
 n, k = map(int, stdin.readline().split())
 length, ans = 0, 0
-arr = [0 for _ in range(10000001)]
+arr = [0 for _ in range(1000000)]
 
 for _ in range(n):
     g, x = map(int, stdin.readline().split())
     length = max(length, x)
     arr[x] = g
 
-p = k * 2 + 1
-res = sum(arr[:p])
+res = sum(arr[:k * 2 + 1])
 ans = res
 
-for i in range(p, length + 1):
-    res += (arr[i] - arr[i - p])
+for i in range(k * 2 + 1, length + 1):
+    res += (arr[i] - arr[i - (k * 2 + 1)])
     ans = max(ans, res)
 
 print(ans)
