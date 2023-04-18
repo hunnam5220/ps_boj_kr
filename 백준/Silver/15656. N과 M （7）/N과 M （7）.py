eefaae7ac82d59a68f6d7afe00079de2b06559d6
juuuun_b <31687036+hunnam5220@users.noreply.github.com>
2023-04-18 10:45:45 +0900

    [Silver III] Title: N과 M (7), Time: 2224 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,19 @@
+from sys import stdin, setrecursionlimit
+
+setrecursionlimit(10 ** 7)
+
+n, m = map(int, stdin.readline().split())
+arr = list(map(int, stdin.readline().split()))
+arr.sort()
+
+
+def f(res, cnt):
+    if cnt == m:
+        print(*res)
+        return
+
+    for i in arr:
+        f(res + [i], cnt + 1)
+
+
+f([], 0)
