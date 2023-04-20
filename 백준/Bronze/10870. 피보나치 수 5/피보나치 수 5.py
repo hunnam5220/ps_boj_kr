a6fb9c0bd8984fe74c4714aad2694698ac8582fa
juuuun_b <31687036+hunnam5220@users.noreply.github.com>
2023-04-20 11:33:44 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,8 @@
+from sys import stdin
+
+n = int(stdin.readline())
+a = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597]
+for i in range(18, 21):
+    a.append(a[-1] + a[-2])
+
+print(a[n])
